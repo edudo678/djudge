d9a214f8166faec90039e463b1698281c44ca9af
eddunic <eddunic@gmail.com>
2019-09-22 17:18:02 -0400

    vizualição de img em pasta predeterminada/

--- src/main/java/tests/NewServlet.java
+++ src/main/java/tests/NewServlet.java
@@ -1,12 +1,21 @@
 package tests;
 
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.logging.Level;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import static jdk.nashorn.internal.objects.NativeError.getFileName;
 import model.bean.Questao;
 import model.bean.QuestaoImagem;
 import model.dao.GenericDAO;
@@ -28,17 +37,56 @@ public class NewServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        Questao q = new Questao();
-        q.setId(1L);
-        QuestaoImagem qi = new QuestaoImagem();
-        qi.setImagem(request.getParameter("upload").getBytes());
-        qi.setQuestao(q);
+        System.out.println(request.getParameter("nome"));
+                
+       
+        response.setContentType("text/html;charset=UTF-8");
+
+    // Create path components to save the file
+    final String path = request.getParameter("destination");
+    final Part filePart = request.getPart("file");
+    final String fileName = (String) getFileName(filePart);
+
+    OutputStream out = null;
+    InputStream filecontent = null;
+    final PrintWriter writer = response.getWriter();
+
+    out = new FileOutputStream(new File(path + File.separator
+            + fileName));
+    filecontent = filePart.getInputStream();
+    int read = 0;
+    final byte[] bytes = new byte[1024];
+    while ((read = filecontent.read(bytes)) != -1) {
+        out.write(bytes, 0, read);
+    }
+    writer.println("New file " + fileName + " created at " + path);
+    LOGGER.log(Level.INFO, "File{0}being uploaded to {1}", 
+                new Object[]{fileName, path});
+    if (out != null) {
+        out.close();
+    }
+    if (filecontent != null) {
+        filecontent.close();
+    }
+    if (writer != null) {
+        writer.close();
+    }
+    
+    
         
-        System.out.println(qi.getImagem());
-
-        GenericDAO<QuestaoImagem> gqi = new GenericDAO<>();
-
-        gqi.saveOrUpdate(qi);
+        
+    }
+    
+    private String getFileName(final Part part) {
+    final String partHeader = part.getHeader("content-disposition");
+    LOGGER.log(Level.INFO, "Part Header = {0}", partHeader);
+    for (String content : part.getHeader("content-disposition").split(";")) {
+        if (content.trim().startsWith("filename")) {
+            return content.substring(
+                    content.indexOf('=') + 1).trim().replace("\"", "");
+        }
+    }
+    return null;
     }
 
 }
